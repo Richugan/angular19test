02adffce27fbccddffa2eb8091dc08410936ca92
Daniel <xcraschx@gmail.com>
2025-04-22 10:34:18 -0700

    mouse chaser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MouseChaserComponent } from "./test/mouse-chaser/mouse-chaser.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    MouseChaserComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'my-dev-project';
--- src/app/test/mouse-chaser/mouse-chaser.component.ts
+++ src/app/test/mouse-chaser/mouse-chaser.component.ts
@@ -1,10 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-mouse-chaser',
-  imports: [],
+  imports: [MatButtonModule, CommonModule],
   templateUrl: './mouse-chaser.component.html',
-  styleUrl: './mouse-chaser.component.scss',
+  styleUrl: './mouse-chaser.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MouseChaserComponent { }
+export class MouseChaserComponent {
+  show = false;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    document.addEventListener('mousemove', (event) => {
+      const mouseX = event.clientX;
+      const mouseY = event.clientY;
+      const element = document.querySelector('#huy-block') as HTMLElement;
+
+      if (element) {
+        element.style.left = `${mouseX - (element.offsetWidth / 2)}px`;
+        element.style.top = `${mouseY - (element.offsetHeight / 2)}px`;
+      }
+    })
+  }
+
+  toggleShow() {
+    this.show = !this.show;
+  }
+}
