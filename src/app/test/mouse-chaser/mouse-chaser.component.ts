d01c591b112e95b3ae8157aa0ddb03cca11293cb
Daniel <xcraschx@gmail.com>
2025-04-23 12:56:59 -0700

    update vars

--- src/app/test/mouse-chaser/mouse-chaser.component.ts
+++ src/app/test/mouse-chaser/mouse-chaser.component.ts
@@ -11,6 +11,9 @@ import { MatButtonModule } from '@angular/material/button';
 })
 export class MouseChaserComponent {
   show = false;
+  mouseHolds = false;
+  
+  mouseBlock: HTMLElement | null = null;
 
   constructor() {
   }
@@ -22,16 +25,59 @@ export class MouseChaserComponent {
     document.addEventListener('mousemove', (event) => {
       const mouseX = event.clientX;
       const mouseY = event.clientY;
-      const element = document.querySelector('#huy-block') as HTMLElement;
+      this.mouseBlock = document.querySelector('#mouse-block') as HTMLElement;
 
-      if (element) {
-        element.style.left = `${mouseX - (element.offsetWidth / 2)}px`;
-        element.style.top = `${mouseY - (element.offsetHeight / 2)}px`;
+      if (this.mouseBlock) {
+        this.mouseBlock.style.left = `${mouseX - (this.mouseBlock.offsetWidth / 2)}px`;
+        this.mouseBlock.style.top = `${mouseY - (this.mouseBlock.offsetHeight / 2)}px`;
       }
     })
+
+    // document.addEventListener('click', (event) => {
+    //   const mouseX = event.clientX;
+    //   const mouseY = event.clientY;
+    //   const element = document.querySelector('#square-block') as HTMLElement;
+
+    //   if (element) {
+    //     element.style.left = `${mouseX - (element.offsetWidth / 2)}px`;
+    //     element.style.top = `${mouseY - (element.offsetHeight / 2)}px`;
+    //   }
+    // })
+
+    document.addEventListener('mousedown', (event) => {
+      this.mouseHolds = true;
+      this.holdMouse(event);
+    })
+    document.addEventListener('mouseup', (event) => {
+      this.mouseHolds = false;
+    })
+  }
+
+  holdMouse(event: MouseEvent) {
+    if(!this.mouseHolds) return;
+    const mouseX = event.clientX;
+    const mouseY = event.clientY;
+    const element = document.querySelector('#square-block') as HTMLElement;
+
+    // if (element) {
+    //   element.style.left = `${mouseX - (element.offsetWidth / 2)}px`;
+    //   element.style.top = `${mouseY - (element.offsetHeight / 2)}px`;
+    // }
+
+    if (element && this.mouseBlock) {
+      element.style.left = `${this.mouseBlock.offsetLeft}px`;
+      element.style.top = `${this.mouseBlock.offsetTop}px`;
+    }
+    
+
+    // console.log(this.mouseBlock?.offsetTop, this.mouseBlock?.offsetLeft);
+    setTimeout(() => this.holdMouse(event), 1);
+
   }
 
   toggleShow() {
     this.show = !this.show;
   }
+
+  
 }
