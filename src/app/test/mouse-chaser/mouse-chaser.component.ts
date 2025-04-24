9c8df7c9a6abf48e57cb35656e4414ffe1e2f69b
Daniel <xcraschx@gmail.com>
2025-04-23 23:42:59 -0700

    now red circle follows your mouse on hold

--- src/app/test/mouse-chaser/mouse-chaser.component.ts
+++ src/app/test/mouse-chaser/mouse-chaser.component.ts
@@ -1,6 +1,20 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { timer } from 'rxjs';
+
+class MouseClass {
+  left: number;
+  top: number;
+  elem: HTMLElement
+
+  constructor(left: number, top: number, elem: HTMLElement) {
+    this.left = left;
+    this.top = top;
+    this.elem = elem
+  }
+}
+
 
 @Component({
   selector: 'app-mouse-chaser',
@@ -12,10 +26,27 @@ import { MatButtonModule } from '@angular/material/button';
 export class MouseChaserComponent {
   show = false;
   mouseHolds = false;
-  
-  mouseBlock: HTMLElement | null = null;
 
-  constructor() {
+  @ViewChild('mouseBlock', { static: false })
+  mouseBlock: ElementRef | null = null;
+
+  @ViewChild('squareBlock', { static: false })
+  squareBlock: ElementRef | null = null;
+
+  animationDelay = 1;
+
+  queue: MouseClass[] = [];
+  isMoving = false;
+  firstMove = true;
+
+  interval: any;
+  ticks = 0;
+
+  movingInterval = 1;
+  cloneColor = '#81ffc5'
+  constructor(
+    protected elementRef: ElementRef,
+  ) {
   }
 
   ngOnInit() {
@@ -25,59 +56,78 @@ export class MouseChaserComponent {
     document.addEventListener('mousemove', (event) => {
       const mouseX = event.clientX;
       const mouseY = event.clientY;
-      this.mouseBlock = document.querySelector('#mouse-block') as HTMLElement;
 
       if (this.mouseBlock) {
-        this.mouseBlock.style.left = `${mouseX - (this.mouseBlock.offsetWidth / 2)}px`;
-        this.mouseBlock.style.top = `${mouseY - (this.mouseBlock.offsetHeight / 2)}px`;
+        this.mouseBlock.nativeElement.style.left = `${mouseX - (this.mouseBlock.nativeElement.offsetWidth / 2)}px`;
+        this.mouseBlock.nativeElement.style.top = `${mouseY - (this.mouseBlock.nativeElement.offsetHeight / 2)}px`;
       }
     })
 
-    // document.addEventListener('click', (event) => {
-    //   const mouseX = event.clientX;
-    //   const mouseY = event.clientY;
-    //   const element = document.querySelector('#square-block') as HTMLElement;
-
-    //   if (element) {
-    //     element.style.left = `${mouseX - (element.offsetWidth / 2)}px`;
-    //     element.style.top = `${mouseY - (element.offsetHeight / 2)}px`;
-    //   }
-    // })
-
     document.addEventListener('mousedown', (event) => {
       this.mouseHolds = true;
-      this.holdMouse(event);
+      this.holdMouse();
     })
     document.addEventListener('mouseup', (event) => {
       this.mouseHolds = false;
     })
   }
 
-  holdMouse(event: MouseEvent) {
-    if(!this.mouseHolds) return;
-    const mouseX = event.clientX;
-    const mouseY = event.clientY;
-    const element = document.querySelector('#square-block') as HTMLElement;
+  holdMouse() {
+    if (!this.mouseHolds) return;
 
-    // if (element) {
-    //   element.style.left = `${mouseX - (element.offsetWidth / 2)}px`;
-    //   element.style.top = `${mouseY - (element.offsetHeight / 2)}px`;
-    // }
+    const mouseX = this.mouseBlock?.nativeElement.offsetLeft + (this.mouseBlock?.nativeElement.offsetWidth / 2);
+    const mouseY = this.mouseBlock?.nativeElement.offsetTop + (this.mouseBlock?.nativeElement.offsetHeight / 2);
 
-    if (element && this.mouseBlock) {
-      element.style.left = `${this.mouseBlock.offsetLeft}px`;
-      element.style.top = `${this.mouseBlock.offsetTop}px`;
+    const clone = this.mouseBlock?.nativeElement.cloneNode(true) as HTMLElement;
+    clone.style.backgroundColor = this.cloneColor;
+    this.elementRef.nativeElement.appendChild(clone);
+
+    this.queue.push(new MouseClass(mouseX, mouseY, clone));
+
+    if (!this.isMoving) {
+      this.updatePos();
     }
-    
 
-    // console.log(this.mouseBlock?.offsetTop, this.mouseBlock?.offsetLeft);
-    setTimeout(() => this.holdMouse(event), 1);
-
+    setTimeout(() => {
+      this.holdMouse();
+    }, this.movingInterval);
   }
+
+  updatePos() {
+    if (this.queue?.length === 0) {
+      this.isMoving = false;
+      return;
+    }
+
+    this.isMoving = true;
+    let shiftedElem = this.queue?.shift(); // get the first element
+
+    const delay = this.firstMove ? 0 : this.animationDelay;
+
+    setTimeout(() => {
+      this.firstMove = false;
+
+      if (this.squareBlock && shiftedElem) {
+        this.squareBlock.nativeElement.style.left = `${shiftedElem.left - (this.squareBlock.nativeElement.offsetWidth / 2)}px`;
+        this.squareBlock.nativeElement.style.top = `${shiftedElem.top - (this.squareBlock.nativeElement.offsetHeight / 2)}px`;
+      }
+
+
+      setTimeout(() => {
+        this.updatePos()
+
+        if (shiftedElem) {
+          shiftedElem.elem.remove()
+        }
+
+      }, this.animationDelay); // 1 сек пауза перед следующим движением
+    }, delay); // Время transition
+  }
+
 
   toggleShow() {
     this.show = !this.show;
   }
 
-  
+
 }
